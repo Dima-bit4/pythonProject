f3f7c5f0d39f328895793c1b933751f55828f622
Admin <dimapetrov05022009@gmail.com>
2023-04-11 12:28:13 +0200

    DZ3

---
+++ main.py
@@ -0,0 +1,27 @@
+class Pet:
+
+    def __init__(self, name, species):
+        self.name = name
+
+        self.species = species
+
+        self.is_alive = True
+
+        self.age = 0
+
+    def grow(self):
+        self.age += 1
+
+
+cat = Pet("Яша", "кошка")
+
+dog = Pet("Шарик", "собака")
+
+cat.grow()
+
+dog.grow()
+
+print(f"{cat.name} растет и ему уже {cat.age} года")
+
+print(f"{dog.name} растет и ему уже {dog.age} года")
+
